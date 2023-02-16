ceb9281b506d4cbbaaabe8e0c793ce1278799d5a
doantruc1989 <doantruc1989@gmail.com>
2023-02-16 18:06:40 +0700

    fix home page

--- pages/components/HeadSeo.tsx
+++ pages/components/HeadSeo.tsx
@@ -14,8 +14,8 @@ function HeadSeo({ prop }: any) {
           name="viewport"
           content="width=device-width, initial-scale=1"
         ></meta>
-        <meta name="keywords" content={seo.keywords || 'tiki'}></meta>
-        <meta name="description" content={seo.description || 'tiki'}></meta>
+        <meta name="keywords" content={seo.keywords || 'tiki clone practice'}></meta>
+        <meta name="description" content={seo.description || 'tiki clone practice'}></meta>
         <meta property="og:title" content={seo.ogTitle || "tiki clone practice"} />
         <meta property="og:type" content={seo.ogType || 'book, phone, iphone, samsung, 13, 14'} />
         <meta property="og:url" content={seo.ogUlr || "https://fetiki2-bt1m-8g05lbo1s-doantruc1989.vercel.app/"} />
--- pages/components/Layout.tsx
+++ pages/components/Layout.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import Nav from "./Nav";
 import Footera from "./Footer";
+import HeadSeo from "./HeadSeo";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
+  const prop = {
+    title: "tiki trang chủ home page tìm kiếm sản phẩm sales off giá rẻ",
+    keywords: "tiki trang chủ home page tìm kiếm sản phẩm sales off giá rẻ",
+    description: "tiki làm trang trang chủ home page tìm kiếm sản phẩm sales off giá rẻ đơn giản easy",
+  }
   return (
     <React.Fragment>
+      <HeadSeo prop={prop}/>
       <Nav />
         <main className=" w-full md:w-11/12 mx-auto">{children}</main>
       <Footera />
