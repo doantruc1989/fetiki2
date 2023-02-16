bd3aa7d1773f7dbdac936bc054fa8fdd19c991b4
doantruc1989 <doantruc1989@gmail.com>
2023-02-16 17:52:09 +0700

    fix seo

--- pages/components/HeadSeo.tsx
+++ pages/components/HeadSeo.tsx
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Head from "next/head";
 
 function HeadSeo({ prop }: any) {
+  const [seo, setSeo] = useState([] as any);
+  useEffect(() => {
+    setSeo(prop)
+  },[])
+
   return (
     <div>
       <Head>
@@ -9,14 +14,14 @@ function HeadSeo({ prop }: any) {
           name="viewport"
           content="width=device-width, initial-scale=1"
         ></meta>
-        <meta name="keywords" content={prop.keywords}></meta>
-        <meta name="description" content={prop.description}></meta>
-        <meta property="og:title" content={prop.ogTitle || "tiki clone practice"} />
-        <meta property="og:type" content={prop.ogType || 'book, phone, iphone, samsung, 13, 14'} />
-        <meta property="og:url" content={prop.ogUlr || "https://fetiki2-bt1m-8g05lbo1s-doantruc1989.vercel.app/"} />
-        <meta property="og:image" content={prop.ogImage || "https://fetiki2-bt1m-8g05lbo1s-doantruc1989.vercel.app/image/logotiki.png"} />
+        <meta name="keywords" content={seo.keywords || 'tiki'}></meta>
+        <meta name="description" content={seo.description || 'tiki'}></meta>
+        <meta property="og:title" content={seo.ogTitle || "tiki clone practice"} />
+        <meta property="og:type" content={seo.ogType || 'book, phone, iphone, samsung, 13, 14'} />
+        <meta property="og:url" content={seo.ogUlr || "https://fetiki2-bt1m-8g05lbo1s-doantruc1989.vercel.app/"} />
+        <meta property="og:image" content={seo.ogImage || "https://fetiki2-bt1m-8g05lbo1s-doantruc1989.vercel.app/image/logotiki.png"} />
         <meta charSet="utf-8"></meta>
-        <title>{prop.title}</title>
+        <title>{seo.title || 'tiki'}</title>
       </Head>
     </div>
   );
